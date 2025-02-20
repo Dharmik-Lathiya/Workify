5270735c797a9101c38e5efea17db678a30a020a
DHARMIK LATHIYA <dharmiklathiya.it@gmail.com>
2025-02-20 22:39:16 +0530

    Select

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Signup from './Pages/Signup';
 import FreelanSignup from './Pages/Freelan/FreelanSignup';
 import CreateProfile from './Pages/Freelan/CreateProfile';
 import { UserDetailsProvider } from './Context/UserDetailsContext.jsx';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Chat from './Components/chat/Chat';
 import FreeLancerHome from './Pages/Freelan/FreeLancerHome.jsx';
@@ -16,18 +16,17 @@ function App() {
   return (
     <UserDetailsProvider>
       <UserProvider>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/freelancer-signup" element={<FreelanSignup />} /> 
-        <Route path='/create-profile' element={<CreateProfile/>} />
-         <Route   path='/chat' element={<Chat/>} />
-         <Route path='/chat/:id' element={<Chat/>}/>     
-        <Route path='/freelancer-dashboard' element={<FreeLancerHome/>} />
-      </Routes>
-         <Route path='/chat/:id' element={<Chat/>}/>
-          <Route path='/notification' element={<Notification/>}/>
-      </Routes>   
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/freelancer-signup" element={<FreelanSignup />} />
+          <Route path='/create-profile' element={<CreateProfile />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='/chat/:id' element={<Chat />} />
+          <Route path='/freelancer-dashboard' element={<FreeLancerHome />} />
+          <Route path='/chat/:id' element={<Chat />} />
+          <Route path='/notification' element={<Notification />} />
+        </Routes>
       </UserProvider>
     </UserDetailsProvider>
   );
